9aa7dc7e2b952039f1de81daa07a3c6b565a9779
kalabkassa <kalabkassa@gmail.com>
2024-03-24 06:40:13 +0300

    prior animation and a couple more touches needed

--- scripts.js
+++ scripts.js
@@ -1,46 +1,48 @@
-const nav = document.querySelector('.nav>ul');
+const nav = document.querySelector(".nav>ul");
 const navToggle = document.querySelector(".mobile-menu");
 
 const tabContainer = document.querySelector(".tabs-container");
 const tabsList = tabContainer.querySelector("ul");
 const tabButtons = tabsList.querySelectorAll("a");
-const tabPanels = tabContainer.querySelectorAll(".panelwrapper");
+const tabPanels = document.querySelectorAll(".panelwrapper");
 
-navToggle.addEventListener('click', () => {
-    const visibility = nav.getAttribute('data-visible');
-    const expanded = navToggle.getAttribute('aria-expanded')
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    const expanded = navToggle.getAttribute("aria-expanded");
 
     console.log(expanded);
-    if (visibility === "false"){
-        nav.setAttribute('data-visible', "true");
-        navToggle.setAttribute('aria-expanded', "true");
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", "true");
+        navToggle.setAttribute("aria-expanded", "true");
+    } else {
+        nav.setAttribute("data-visible", "false");
+        navToggle.setAttribute("aria-expanded", "false");
     }
-    else {
-        nav.setAttribute('data-visible', "false");
-        navToggle.setAttribute('aria-expanded', "false");
-    }
-})
+});
 
 tabButtons.forEach((tab, index) => {
-    if(index ===0){
-
+    if (index === 0) {
     } else {
         tabPanels[index].setAttribute("hidden", "");
+        console.log(tabPanels[index]);
     }
-})
+});
 
 tabContainer.addEventListener("click", (e) => {
     const clickedTab = e.target.closest("a");
-    if(!clickedTab) return;
+    if (!clickedTab) return;
     e.preventDefault();
 
     const activePanelId = clickedTab.getAttribute("href");
-    const activePanel = tabContainer.querySelector(activePanelId);
+    const activePanel = document.querySelector(activePanelId);
 
-    tabPanels.forEach((panel) => {
+    tabPanels.forEach((panel, index) => {
         panel.setAttribute("hidden", true);
         panel.setAttribute("aria-expanded", "false");
+        tabButtons[index].setAttribute("aria-expanded", "false");
     });
+
     activePanel.removeAttribute("hidden", false);
-    activePanel.setAttribute("aria-expanded", "true")
-})
+    activePanel.setAttribute("aria-expanded", "true");
+    clickedTab.setAttribute("aria-expanded", "true");
+});
